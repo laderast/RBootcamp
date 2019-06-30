ae31c9618241d7f32e6ebe16445d324164343e84
Ted Laderas <roshioshi@gmail.com>
2019-06-30 16:22:18 -0700

    update exercise 02_02

--- exercises/exc_02_02.R
+++ exercises/exc_02_02.R
@@ -1,8 +1,9 @@
 library(ggplot2)
-pets <- readRDS("data/pets.rds")
+pets <- read.csv("data/pets.csv")
+
 ##Show a barplot and count by name and fill by animal
 ##theme() allows us to angle the text labels so that we can read them
-ggplot(pets, aes(x=   )) + geom_bar() + 
+ggplot(pets, aes(x= -----)) + geom_bar() + 
     ##We make the x axis text angled 
     ##for better legibility
     theme(axis.text.x = element_text(angle=45))
--- exercises/solution_02_02.R
+++ exercises/solution_02_02.R
@@ -2,7 +2,9 @@ library(ggplot2)
 pets <- readRDS("data/pets.rds")
 
 ##show a barplot and count by name and fill by animal
+##theme() allows us to angle the text labels so that we can read them
 ggplot(pets, aes(x=name)) + geom_bar() + 
-    ##we make the x axis text angled for better legibility
+    ##we make the x axis text angled 
+    ##for better legibility
     theme(axis.text.x = element_text(angle=45))
 
