7a2b49640dd298a1a05a9d6124efc6db4cfc8966
Ted Laderas <roshioshi@gmail.com>
2019-07-01 20:15:25 -0700

    update 02_03

--- exercises/exc_02_03.R
+++ exercises/exc_02_03.R
@@ -1,5 +1,6 @@
 library(ggplot2)
-pets <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_3864/datasets/pets.csv")
+pets <- read.csv("data/pets.csv")
+
 #map the right variable in pets to fill
-ggplot(pets, aes(x=animal, fill= )) + 
+ggplot(pets, aes(x=animal, fill= ----)) + 
   geom_bar(color="black")
