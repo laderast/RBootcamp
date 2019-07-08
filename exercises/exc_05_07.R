fc2afa20264aa2ad2100e387cf040123f317d8ba
Jessica Minnier <jessica.minnier@gmail.com>
2019-07-08 12:27:13 -0700

    update 5_7

--- exercises/exc_05_07.R
+++ exercises/exc_05_07.R
@@ -1,12 +1,14 @@
-library(tidyverse)
-library(broom)
+library(dplyr)
+library(ggplot2)
 
-fishdata <- read_csv("data/fisherman_mercury_modified.csv")
-fishdata$fisherman <- factor(fishdata$fisherman)
+fishdata <- read.csv("data/fisherman_mercury_modified.csv") %>%
+  mutate(fisherman = factor(fisherman))
 
-# draw a scatterplot of weight (x-axis) vs total_mercury (y axis) and color by fisherman category
+# draw a scatterplot of weight (x-axis) vs total_mercury (y axis) 
+# and color by fisherman category
 ggplot(fishdata,aes(___,___,color=___))+___
 
-# draw a scatterplot of fishmlwk (x-axis) vs total_mercury (y axis) and color by fisherman category
+# draw a scatterplot of fishmlwk (x-axis) vs total_mercury (y axis) 
+# and color by fisherman category
 ggplot(fishdata,aes(___,___,color=___))+___
 
--- exercises/solution_05_07.R
+++ exercises/solution_05_07.R
@@ -1,11 +1,15 @@
-library(tidyverse)
-library(broom)
+library(dplyr)
+library(ggplot2)
 
-fishdata <- read_csv("data/fisherman_mercury_modified.csv")
-fishdata$fisherman <- factor(fishdata$fisherman)
+fishdata <- read.csv("data/fisherman_mercury_modified.csv") %>%
+  mutate(fisherman = factor(fisherman))
 
-# draw a scatterplot of weight (x-axis) vs total_mercury (y axis) and color by fisherman category
-ggplot(fishdata,aes(x=weight,y=total_mercury,color=fisherman))+geom_point()
+# draw a scatterplot of weight (x-axis) vs total_mercury (y axis) 
+# and color by fisherman category
+ggplot(fishdata,aes(x=weight,y=total_mercury,color=fisherman))+
+  geom_point()
 
-# draw a scatterplot of fishmlwk (x-axis) vs total_mercury (y axis) and color by fisherman category
-ggplot(fishdata,aes(x=fishmlwk,y=total_mercury,color=fisherman))+geom_point()
+# draw a scatterplot of fishmlwk (x-axis) vs total_mercury (y axis) 
+# and color by fisherman category
+ggplot(fishdata,aes(x=fishmlwk,y=total_mercury,color=fisherman))+
+  geom_point()
