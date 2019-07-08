0e2f7e4cb04adf6da068a88593beafb0018901b9
Jessica Minnier <jessica.minnier@gmail.com>
2019-07-08 13:20:55 -0700

    update 5_13

--- exercises/exc_05_13.R
+++ exercises/exc_05_13.R
@@ -1,24 +1,24 @@
-library(tidyverse)
+library(dplyr)
 library(broom)
 
-fishdata <- read_csv("data/fisherman_mercury_modified.csv")
-fishdata$fisherman <- factor(fishdata$fisherman)
+fishdata <- read.csv("data/fisherman_mercury_modified.csv") %>%
+  mutate(fisherman = factor(fisherman))
 
 # Here are our models again
 fit_univariate <- lm(total_mercury~fisherman,data=fishdata)
 fit_multiple <- lm(total_mercury~fisherman+weight+fishmlwk,data=fishdata)
 
 # save augmented data here
-fit_multiple_augment <- 
-fit_univariate_augment <- 
+fit_multiple_augment <- ____
+fit_univariate_augment <- ____
 
 # bind rows
-augmented_data <- bind_rows("univariate" = ,
-                              "multiple" = ,
+augmented_data <- bind_rows("univariate" = ____,
+                              "multiple" = ____,
                               .id = "model")
 
 # scatterplot of total mercury (x-axis) vs fitted values (y-axis), color fishmlwk and shape fisherman
-ggplot(augmented_data, aes()) +
+ggplot(augmented_data, aes(____)) +
   geom_point() +
-  geom_abline() +
-  facet_wrap()
+  geom_abline(____, ____) +
+  facet_wrap(~____)
