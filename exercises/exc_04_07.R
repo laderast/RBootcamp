0f6e5cf1d7aa6f715c46333ad67cc1397b43efbb
Ted Laderas <roshioshi@gmail.com>
2019-07-12 22:30:26 -0700

    Update exc_04_07.R

--- exercises/exc_04_07.R
+++ exercises/exc_04_07.R
@@ -6,6 +6,8 @@ MouseBalanceTimeSeries <- read.csv("data/mouse.csv")
 gatheredMouse <- MouseBalanceTimeSeries %>% 
   gather(-----) %>%
   filter(!is.na(time)) %>% 
+  #we have to recast time as numeric here
+  mutate(time = as.numeric(time)) %>%
   separate(-----)
 
 ggplot(gatheredMouse, aes(x=intervention, y=time)) + geom_boxplot()
