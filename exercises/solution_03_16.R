3c9775156cc6edc7d641c84ccdbd6553f114a8bb
Ted Laderas <roshioshi@gmail.com>
2019-07-13 18:10:34 -0700

    Update solution_03_16.R

--- exercises/solution_03_16.R
+++ exercises/solution_03_16.R
@@ -2,6 +2,6 @@ library(dplyr)
 biopics <- readRDS("data/biopics.rds")
 
 biopics_sorted <- biopics %>% 
-  arrange(year_release, lead_actor_actress)
+  arrange(year_release, box_office)
 
 head(biopics_sorted)
